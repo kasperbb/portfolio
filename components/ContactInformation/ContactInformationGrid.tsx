4f257524543722a059cb67009bc99e9f8fe0dea7
kasperbb <kasperbb98@gmail.com>
2022-01-24 19:16:40 +0100

    Adds escaped apostrophes

--- components/ContactInformation/ContactInformationGrid.tsx
+++ components/ContactInformation/ContactInformationGrid.tsx
@@ -9,7 +9,7 @@ interface ContactInformationGridProps {
 export const ContactInformationGrid: FC<ContactInformationGridProps> = ({ contactInformation: { email, phone, linkedIn, cv } }) => {
 	return (
 		<>
-			<h3 className="text-3xl font-bold lg:text-4xl mb-14 text-accent font-heading">Let's work together!</h3>
+			<h3 className="text-3xl font-bold lg:text-4xl mb-14 text-accent font-heading">Let&#39;s work together!</h3>
 			<div className="grid grid-cols-1 lg:max-w-2xl sm:grid-cols-2 gap-14 gap-y-10">
 				<ContactInformation {...email} />
 				<ContactInformation {...phone} />
--- components/Sections/ProjectsSection.tsx
+++ components/Sections/ProjectsSection.tsx
@@ -11,7 +11,7 @@ export const ProjectsSection: FC<ProjectsSectionProps> = ({ projects }) => {
 		<main className="relative py-20 bg-gray-100 text-accent" id="works">
 			<div className="px-5 mx-auto max-w-7xl">
 				<div className="items-center justify-between lg:flex mb-14 font-heading">
-					<h2 className="text-3xl font-bold text-center lg:text-5xl lg:text-left">What's Happening?</h2>
+					<h2 className="text-3xl font-bold text-center lg:text-5xl lg:text-left">What&#39;s Happening?</h2>
 					<a
 						href="https://github.com/kasperbb"
 						className="hidden px-3 py-2 text-sm font-semibold leading-none text-white uppercase transition duration-500 rounded lg:block bg-primary hover:bg-primary-light"
